ec427d35d091d3422da5290eb1896870ff833b26
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-08-13 01:06:29 +0200

    Implementing Advanced Model Relationships in Django

---
+++ django-models/LibraryProject/README.md
@@ -0,0 +1,1 @@
+Alx and Django intro
---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,27 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def query_all_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return f"No author found with name {author_name}"
+
+def list_all_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        return books
+    except Library.DoesNotExist:
+        return f"No library found with name {library_name}"
+
+def retrieve_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library)
+        return librarian
+    except Library.DoesNotExist:
+        return f"No library found with name {library_name}"
+    except Librarian.DoesNotExist:
+        return f"No librarian found for library {library_name}"
