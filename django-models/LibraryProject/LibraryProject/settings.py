95d4427efcabf0c4423f051b856bea0fca96ee9b
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-08-15 20:46:22 +0200

    first commit

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "relationship_app.apps.RelationshipAppConfig",
     "bookshelf.apps.BookshelfConfig",
     "django.contrib.admin",
     "django.contrib.auth",
