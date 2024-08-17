987f45609162996a00ad2f14c2644471670b1ab0
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-08-17 21:32:31 +0200

    relationship app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -21,7 +21,7 @@ class LibraryDetailView(DetailView):
     
 class RegisterView(FormView):
     template_name = 'relationship_app/register.html'
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
