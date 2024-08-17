67ec0939ec6b7b85338a0e08f37c69cebb847240
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-08-17 18:10:04 +0200

    relationship app

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -9,5 +9,5 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = 'relationship_app/list_books.html'
     context_object_name = 'library'
