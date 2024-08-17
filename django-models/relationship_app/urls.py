4ae2ccb70bb23b5d8059dd92fc7575d6f24f2aa4
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-08-17 21:18:31 +0200

    relationship app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import (LibraryDetailView, LoginView, LogoutView, RegisterView,
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(template_name='login'),),
+    path('logout/', LogoutView.as_view(template_name='logout'),),
+    path('register/', RegisterView.views.register(), name='register'),
 ]
