bb0fc79ce4302576bf1e9c2917dd6628f5969b53
Mhlengi Ngubane <87973826+MhlengiNgubane@users.noreply.github.com>
2024-09-18 03:03:39 +0200

    social_media_api

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -31,7 +31,7 @@ class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        user_to_follow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.add(user_to_follow)
         return Response({'message': f'You are now following {user_to_follow.username}'})
 
@@ -39,6 +39,6 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        user_to_unfollow = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({'message': f'You have unfollowed {user_to_unfollow.username}'})
